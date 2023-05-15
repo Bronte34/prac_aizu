f71b7df6d237900958de287721bb1a88a0034522
Bronte34 <80271697+Bronte34@users.noreply.github.com>
2023-05-15 15:23:29 +0900

    Add files via upload

--- No_22_finding_missing_cards.c
+++ No_22_finding_missing_cards.c
@@ -4,24 +4,22 @@ int main(void)
 {
 	int n, i, j, number;
 	int number_of_rows, number_of_columns;
-	char unsigned suit, number_to_suit[4] = {'S', 'H', 'C', 'D'};
 	int cards[4][13];
+	char unsigned suit;
+	char number_to_suit[4] = {'S', 'H', 'C', 'D'};
 	
 	/*Initialize arr with 0*/
 	number_of_rows = sizeof(cards) / sizeof(cards[0]); 
 	number_of_columns = sizeof(cards[0]) / sizeof(cards[0][0]);
-
 	for (i = 0; i < number_of_rows; i++) {
 		for (j = 0; j < number_of_columns; j++) {
 			cards[i][j] = 0; 
 		}
 	}
-	
 
 	scanf("%d", &n);
-
 	for (i = 0; i < n; i++) {
-		scanf("%c %d", &suit, &number);
+		scanf("%*c %c %d", &suit, &number); // %*c is read and ignore 1 character 
 		switch (suit) {
 			case 'S':
 				suit = 0;
@@ -36,8 +34,7 @@ int main(void)
 				suit = 3;
 				break;
 		}
-
-		cards[suit][number] = 1;
+		cards[suit][number-1] = 1;
 	}
 
 	for (i = 0; i < number_of_rows; i++) {
